a4dbd1ae3e50d7007e787fee35e873771a883877
Mark Black <mark.prestwick@gmail.com>
2016-06-30 08:40:08 -0500

    Demo modification
    Description of my change

--- test/example-test.js
+++ test/example-test.js
@@ -8,6 +8,6 @@ describe('sumAndDif', function () {
     var result = example(1, 2);
 
     it('Should Sum', function () {
-        expect(result.sum).toBe(3);
+        expect(result.sum).toBe(4);
     });
 });
