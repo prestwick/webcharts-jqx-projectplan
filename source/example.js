b5337ee1371087b168667642edb63ef844f0709b
Mark Black <mark.prestwick@gmail.com>
2018-03-16 10:10:46 -0500

    code change

--- source/example.js
+++ source/example.js
@@ -7,7 +7,7 @@ var example = function (firstNum, secondNum) {
     };
 };
 
-var goToProd1 = function (firstNum, secondNum) {
+var andAgain = function (firstNum, secondNum) {
     'use strict';
     var sum;
     sum = firstNum + secondNum;
